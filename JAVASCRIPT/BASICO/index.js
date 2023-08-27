c260e858c34a840f6eaab448f3c6b1b20a02f024
RICHARD SASVIN <richard.ortiz@digicom.com.gt>
2023-08-27 16:01:05 -0600

    js basico

---
+++ JAVASCRIPT/BASICO/index.js
@@ -0,0 +1,45 @@
+<script type="text/javascript">
+
+
+</script>
+
+//para los comentarios se maneja el slash
+/**
+ * COMENTARIO QUE ES MULTILINEAS
+ * PARA LLEVAR UN MEJOR CONTROL
+ * A NIVEL DOCUMENTAL
+ */
+
+//TUPOS DE DATOS
+function tiposdedatos(){
+  //tipo de datos principales
+  //tipos primitivos
+  var num = 16;   //variable llamada num tiene un valor de 16
+  var cad= "hola";  //cadena (string)
+  var bo = false;  //boleano
+  var y;  //un tipo de cadena indefinido (undefined)
+  var w = null;   //null (objeto para referencias)
+
+  //tipos complejos
+  var obj = {nombre: "Juan", edad: 16};  //objeto
+  var func= function unafuncion(){} //function
+
+
+  //typeof nos dice que tipo de dato conteine una variable
+  typeof num //resutlado es number
+  typeof cad //resutlado es string
+  typeof bo //resultado booleano
+  typeof y //resultado undefined
+  typeof w // restultado es un object
+  typeof obj //restultado es object
+  typeof func //resultado ers una function
+
+  bo = unidefined;  //la variable no tiene ahora ni valor ni tipo
+
+
+}
+
+//OPERADORES MATEMATICOS, LOGICOS Y DE COMPARACION
+function operadores(){
+  var a = 2;
+}
