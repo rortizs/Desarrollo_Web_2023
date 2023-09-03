356412f4658c1b4e7f4b4515f82320b5a664745d
RICHARD SASVIN <richard.ortiz@digicom.com.gt>
2023-09-03 15:41:04 -0600

    modified index.html

--- PRACTICA/EJERCICIO1/JS/main.js
+++ PRACTICA/EJERCICIO1/JS/main.js
@@ -1,10 +1,7 @@
-var respuesta = prompt("Cual es la capital de españa?");
+var respuesta = prompt("¿Cuál es la capital de España?");
 
-// Uso de la funcion toLwerCase(), sirve para convertir la respuesta
-// a minusculas y que haya problema al comparar con el string "madrid"
-if (respuesta.toLowerCase() === "madrid") {
-  document.write("Correcto, Madrid es la capital de España!");
-}else{
-  document.write('Su respuesta, \"' + respuesta + '\", no es correcta.');
-
-}
+// Uso la función toLowerCase() para convertir la respuesta
+// a minúsculas y que no haya problema al comparar con el string "madrid"
+if (respuesta.toLowerCase() === "madrid")
+  document.write("¡Correcto, Madrid es la capital de España!");
+else document.write('Su respuesta, "' + respuesta + '", no es correcta.');
